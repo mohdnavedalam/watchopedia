1b2b1ec6374ea36d62b66a5aed202c4c530cef90
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-06-06 03:38:55 +0530

    etup counter

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
+import Counter from './Components/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <div>
     <Header />
-     
+    <div className='p-2 m-2 row text-center'>
+      <Counter />
+    </div>
     <Footer />
   </div>
 );
