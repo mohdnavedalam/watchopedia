42d5be413f6788311bb3b10c47600d78b68d0612
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-06-06 04:25:58 +0530

    state with objects - 2

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -2,21 +2,21 @@ import { useState } from "react";
 
 const Counter = () => {
     const [counterState, setCounterState] = useState(() => {
-        return {counter: 10};
+        return {counter: 10, title: "Yo!"};
     });
     function handleIncrement () {
         setCounterState((prevState) => {
-            return {counter: prevState.counter + 1}
+            return {...prevState, counter: prevState.counter + 1}
         });
     }
     function handleDecrement () {
         setCounterState((prevState) => {
-            return {counter: prevState.counter - 1}
+            return {...prevState, counter: prevState.counter - 1}
         });
     }
     return (
         <div className="col-12 col-md-4 offset-md-4 border">
-            <span className="h2 pt-4 m-2">Fun Counter</span>
+            <span className="h2 pt-4 m-2">{counterState.title} &nbsp; Fun Counter</span>
             <br />
             <button className="btn btn-success m-1" onClick={handleIncrement}>+1</button>
             <button className="btn btn-danger m-1" onClick={handleDecrement}>-1</button>
