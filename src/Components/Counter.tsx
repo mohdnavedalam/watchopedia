a52900c5278515ee10f074083014387d2e9e7c20
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-06-06 04:08:40 +0530

    usestate in action - 2

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [counter, setCounter] = useState(10);
+    const [counter, setCounter] = useState(() => {
+        return 10;
+    });
+    function handleIncrement () {
+        setCounter((prevState) => {
+            return prevState + 1;
+        });
+    }
+    function handleDecrement () {
+        setCounter((prevState) => {
+            return prevState - 1;
+        });
+    }
     return (
         <div className="col-12 col-md-4 offset-md-4 border">
             <span className="h2 pt-4 m-2">Fun Counter</span>
             <br />
-            <button className="btn btn-success m-1">+1</button>
-            <button className="btn btn-danger m-1">-1</button>
+            <button className="btn btn-success m-1" onClick={handleIncrement}>+1</button>
+            <button className="btn btn-danger m-1" onClick={handleDecrement}>-1</button>
             <br />
             <span className="h4">
                 Counter: &nbsp;
